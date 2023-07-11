b12fbf7427d9da5a73a006fbfa146dc765d83a2f
Your Name <al1m.budaev@yandex.ru>
2023-07-11 13:37:47 +0300

    feat: init books component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,13 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import Books from './components/Books.vue';
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+ <Books/>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 header {
   line-height: 1.5;
   max-height: 100vh;
---
+++ src/api/db.js
@@ -0,0 +1,78 @@
+const DB_NAME = 'bookdb'
+const STORAGE_NAME = 'books'
+const DB_VERSION = 1
+let DB
+
+export default {
+  async getDb () {
+    return new Promise((resolve, reject) => {
+      if (DB) {
+        return resolve(DB)
+      }
+      const request = window.indexedDB.open(DB_NAME, DB_VERSION)
+
+      request.onerror = e => {
+        console.log('Error opening db', e)
+        // eslint-disable-next-line prefer-promise-reject-errors
+        reject('Error')
+      }
+
+      request.onsuccess = e => {
+        DB = e.target.result
+        resolve(DB)
+      }
+
+      request.onupgradeneeded = e => {
+        let db = e.target.result
+        db.createObjectStore(STORAGE_NAME, { autoIncrement: true, keyPath: 'id' })
+      }
+    })
+  },
+  async deleteBook (book) {
+    const db = await this.getDb()
+
+    return new Promise(resolve => {
+      const trans = db.transaction([STORAGE_NAME], 'readwrite')
+      trans.oncomplete = () => {
+        resolve()
+      }
+
+      const store = trans.objectStore(STORAGE_NAME)
+      store.delete(book.id)
+    })
+  },
+  async getBooks () {
+    let db = await this.getDb()
+
+    return new Promise(resolve => {
+      let trans = db.transaction([STORAGE_NAME], 'readonly')
+      trans.oncomplete = () => {
+        resolve(books)
+      }
+
+      const store = trans.objectStore(STORAGE_NAME)
+      const books = []
+
+      store.openCursor().onsuccess = e => {
+        const cursor = e.target.result
+        if (cursor) {
+          books.push(cursor.value)
+          cursor.continue()
+        }
+      }
+    })
+  },
+  async saveBook (book) {
+    let db = await this.getDb()
+
+    return new Promise(resolve => {
+      let trans = db.transaction([STORAGE_NAME], 'readwrite')
+      trans.oncomplete = () => {
+        resolve()
+      }
+
+      let store = trans.objectStore(STORAGE_NAME)
+      store.put(book)
+    })
+  }
+}
